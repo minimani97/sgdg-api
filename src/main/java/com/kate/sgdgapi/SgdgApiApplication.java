607990366886dfa76720ad3ac294ccd0ee42f42a
minimani97 <shineeworld408@gmail.com>
2020-05-25 14:59:50 +0900

    project initialize

---
+++ src/main/java/com/kate/sgdgapi/SgdgApiApplication.java
@@ -0,0 +1,13 @@
+package com.kate.sgdgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgdgApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgdgApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kate/sgdgapi/SgdgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kate.sgdgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgdgApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
